2ce69d200fcafeda033f999c5b628a5d3a279671
aarnhub <49189897+aarnhub@users.noreply.github.com>
2019-04-02 12:49:08 +0300

    Add files via upload

---
+++ bulksms2.py
@@ -0,0 +1,35 @@
+import urllib
+import time
+# Proovl SMS API settings www.proovl.com / Script for Python 2
+user = "***********"   # change ***** to your Proovl user ID
+token = "***********"   # change ***** to your Proovl token
+from1 = "***********"  # change ***** to your Proovl SMS number
+text = "Hello World"   # text
+# Add numbers: format "44755555555" / one per line
+numbers = [
+"44755555555",
+"44755555555",
+"44755555555",
+"44755555555"
+]
+messagesSent = 0
+host = "https://www.proovl.com/api/send.php?"
+for x in numbers:
+  messagesSent += 1
+  params = {
+  "user": user,       
+  "token": token,
+  "from": from1,
+  "text": text,
+  "to": x}
+  query_string = urllib.urlencode(params)   
+  http_req = host + query_string
+  f = urllib.urlopen(http_req)
+  txt = (f.read().decode('utf-8'))
+  z = txt.split(";")
+  time.sleep(0.5)
+  print("Progress: {}/{}") .format(messagesSent, len(numbers)), (x), (z[1])
+if z[0] == "Error":
+  print("== Error. Text messages not sent ==")
+else:
+  print("== All messages has been sent! ==")
---
+++ bulksms3.py
@@ -0,0 +1,45 @@
+import urllib
+import urllib.parse
+import urllib.request
+import time
+from urllib import request as urlrequest
+import ssl
+# Proovl SMS API settings www.proovl.com / Script for Python 3+
+user = "***********"   # change ***** to your Proovl user ID
+token = "***********"   # change ***** to your Proovl token
+from1 = "***********"  # change ***** to your Proovl SMS number
+text = "Hello World"   # text
+# Add numbers: format "44755555555" / one per line
+numbers = [
+"44755555555",
+"44755555555",
+"44755555555",
+"44755555555"
+]
+messagesSent = 0
+host = "https://www.proovl.com/api/send.php?"
+for x in numbers:
+  messagesSent += 1
+  params = {
+  "user": user,       
+  "token": token,
+  "from": from1,
+  "text": text,
+  "to": x}
+  try:
+    _create_unverified_https_context = ssl._create_unverified_context
+  except AttributeError:
+    pass
+  else:
+    ssl._create_default_https_context = _create_unverified_https_context
+  query_string = urllib.parse.urlencode(params)   
+  http_req = host + query_string
+  f = urllib.request.urlopen(http_req)
+  txt = (f.read().decode('utf-8'))
+  z = txt.split(";")
+  time.sleep(0.5)
+  print("Progress: {}/{}".format(messagesSent, len(numbers)), (x), (z[1]))
+if z[0] == "Error":
+  print("== Error. Text messages not sent ==")
+else:
+  print("== All messages has been sent! ==")
